6151467faf7d6084c3c6030921ba8eeb2943c329
GSreniawska <gabriela.sreniawska@gmail.com>
2019-12-02 16:23:27 +0100

    Add Data Objects

---
+++ src/main/java/Data/JavaFile.java
@@ -0,0 +1,35 @@
+package Data;
+
+public class JavaFile {
+    private String javaFileName;
+    private int size;
+
+    public JavaFile(String javaFileName, int size) {
+        this.javaFileName = javaFileName;
+        this.size = size;
+    }
+
+    public String getJavaFileName() {
+        return javaFileName;
+    }
+
+    public void setJavaFileName(String javaFileName) {
+        this.javaFileName = javaFileName;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "JavaFile{" +
+                "javaFileName='" + javaFileName + '\'' +
+                ", size=" + size +
+                '}';
+    }
+}

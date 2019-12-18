a5dd409d245dfbc8956d315e7932a850e2d03de4
NWojt <n.wojtowicz098@gmail.com>
2019-12-18 23:54:13 +0100

    Feature # XML export API call # API XML model

--- src/main/java/SpringApplication/GraphApplicationController.java
+++ src/main/java/SpringApplication/GraphApplicationController.java
@@ -5,13 +5,27 @@ import ApiData.DirPath;
 import ApiData.Edge;
 import ApiData.Node;
 import Data.AllData;
+import ApiData.ApiXmlModel;
 import Data.DataGuide;
+import Export.XMLFileBuilder;
 import com.google.gson.Gson;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
-
+import java.util.Properties;
 
 
 @RestController
@@ -19,7 +33,7 @@ public class GraphApplicationController {
 
     @CrossOrigin(origins = "http://localhost:8080")
     @ResponseBody
-    @RequestMapping(path="/file_file", method = RequestMethod.POST)
+    @RequestMapping(path = "/file_file", method = RequestMethod.POST)
     public ResponseEntity dirPath(@RequestBody String path) {
         Gson gson = new Gson();
         DirPath dir = gson.fromJson(path, DirPath.class);
@@ -37,14 +51,14 @@ public class GraphApplicationController {
         ArrayList<Node> tempNodes = new ArrayList<Node>();
         ArrayList<Edge> tempEdge = new ArrayList<Edge>();
 
-        for(int i=0;i<allData.getListOfJavaFiles().size();i++){
+        for (int i = 0; i < allData.getListOfJavaFiles().size(); i++) {
             tempNodes.add(new Node(allData.getListOfJavaFiles().get(i)));
         }
-        for(int i =0;i<allData.getListOfEdgesFile_File().size();i++){
+        for (int i = 0; i < allData.getListOfEdgesFile_File().size(); i++) {
             tempEdge.add(new Edge(allData.getListOfEdgesFile_File().get(i)));
         }
 
-        ApiData temp = new ApiData(tempNodes,tempEdge);
+        ApiData temp = new ApiData(tempNodes, tempEdge);
 
         return new ResponseEntity(gson.toJson(temp), HttpStatus.OK);
 
@@ -52,7 +66,7 @@ public class GraphApplicationController {
 
     @CrossOrigin(origins = "http://localhost:8080")
     @ResponseBody
-    @RequestMapping(path="/method_method", method = RequestMethod.POST)
+    @RequestMapping(path = "/method_method", method = RequestMethod.POST)
     public ResponseEntity methodToMethod(@RequestBody String path) {
         Gson gson = new Gson();
         DirPath dir = gson.fromJson(path, DirPath.class);
@@ -71,14 +85,14 @@ public class GraphApplicationController {
         ArrayList<Node> tempNodes = new ArrayList<Node>();
         ArrayList<Edge> tempEdge = new ArrayList<Edge>();
 
-        for(int i=0;i<allData.getListOfMethods().size();i++){
+        for (int i = 0; i < allData.getListOfMethods().size(); i++) {
             tempNodes.add(new Node(allData.getListOfMethods().get(i)));
         }
-        for(int i =0;i<allData.getListOfEdgesMethod_Method().size();i++){
+        for (int i = 0; i < allData.getListOfEdgesMethod_Method().size(); i++) {
             tempEdge.add(new Edge(allData.getListOfEdgesMethod_Method().get(i)));
         }
 
-        ApiData temp = new ApiData(tempNodes,tempEdge);
+        ApiData temp = new ApiData(tempNodes, tempEdge);
 
         return new ResponseEntity(gson.toJson(temp), HttpStatus.OK);
 
@@ -86,7 +100,7 @@ public class GraphApplicationController {
 
     @CrossOrigin(origins = "http://localhost:8080")
     @ResponseBody
-    @RequestMapping(path="/method_package", method = RequestMethod.POST)
+    @RequestMapping(path = "/method_package", method = RequestMethod.POST)
     public ResponseEntity methodToPackage(@RequestBody String path) {
         Gson gson = new Gson();
         DirPath dir = gson.fromJson(path, DirPath.class);
@@ -105,17 +119,17 @@ public class GraphApplicationController {
         ArrayList<Node> tempNodes = new ArrayList<Node>();
         ArrayList<Edge> tempEdge = new ArrayList<Edge>();
 
-        for(int i=0;i<allData.getListOfMethods().size();i++){
+        for (int i = 0; i < allData.getListOfMethods().size(); i++) {
             tempNodes.add(new Node(allData.getListOfMethods().get(i)));
         }
-        for(int i =0;i<allData.getListOfPackages().size();i++){
+        for (int i = 0; i < allData.getListOfPackages().size(); i++) {
             tempNodes.add(new Node(allData.getListOfPackages().get(i)));
         }
-        for(int i =0;i<allData.getListOfEdgesFile_File().size();i++){
+        for (int i = 0; i < allData.getListOfEdgesFile_File().size(); i++) {
             tempEdge.add(new Edge(allData.getListOfEdgesFile_File().get(i)));
         }
 
-        ApiData temp = new ApiData(tempNodes,tempEdge);
+        ApiData temp = new ApiData(tempNodes, tempEdge);
 
         return new ResponseEntity(gson.toJson(temp), HttpStatus.OK);
 
@@ -123,7 +137,7 @@ public class GraphApplicationController {
 
     @CrossOrigin(origins = "http://localhost:8080")
     @ResponseBody
-    @RequestMapping(path="/package_package", method = RequestMethod.POST)
+    @RequestMapping(path = "/package_package", method = RequestMethod.POST)
     public ResponseEntity packageToPackage(@RequestBody String path) {
         Gson gson = new Gson();
         DirPath dir = gson.fromJson(path, DirPath.class);
@@ -141,17 +155,118 @@ public class GraphApplicationController {
         ArrayList<Node> tempNodes = new ArrayList<Node>();
         ArrayList<Edge> tempEdge = new ArrayList<Edge>();
 
-        for(int i=0;i<allData.getListOfPackages().size();i++){
+        for (int i = 0; i < allData.getListOfPackages().size(); i++) {
             tempNodes.add(new Node(allData.getListOfPackages().get(i)));
         }
-        for(int i =0;i<allData.getListOfEdgesPackage_Package().size();i++){
+        for (int i = 0; i < allData.getListOfEdgesPackage_Package().size(); i++) {
             tempEdge.add(new Edge(allData.getListOfEdgesPackage_Package().get(i)));
         }
 
-        ApiData temp = new ApiData(tempNodes,tempEdge);
+        ApiData temp = new ApiData(tempNodes, tempEdge);
 
         return new ResponseEntity(gson.toJson(temp), HttpStatus.OK);
 
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
+    @ResponseBody
+    @RequestMapping(path = "xml_export", method = RequestMethod.GET)
+    public ResponseEntity xmlExport(@RequestBody String path) {
+        Gson gson = new Gson();
+        DirPath dir = gson.fromJson(path, DirPath.class);
+
+        AllData allData = new AllData();
+        DataGuide dataGuide = new DataGuide();
+
+        ApiXmlModel model = new ApiXmlModel();
+
+        try {
+            dataGuide.findModuleDependencies(allData);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity(null, HttpStatus.BAD_REQUEST);
+        }
+
+        XMLFileBuilder xmlFileBuilder = new XMLFileBuilder();
+
+        File files = new File("files.xml");
+        File methods = new File("methods.xml");
+        File modules = new File("modules.xml");
+
+        try {
+            xmlFileBuilder.addElements(dataGuide.getFileOneFileTwoWeight());
+            XMLBuilder2 builderOne = xmlFileBuilder.getBuilder();
+            PrintWriter writerOne = new PrintWriter(files);
+            Properties propertiesOne = xmlFileBuilder.getProperties();
+            builderOne.toWriter(writerOne, propertiesOne);
+
+            xmlFileBuilder.addElements(dataGuide.getMethodOneMethodTwoWeight());
+            XMLBuilder2 builderTwo = xmlFileBuilder.getBuilder();
+            PrintWriter writerTwo = new PrintWriter(methods);
+            Properties propertiesTwo = xmlFileBuilder.getProperties();
+            builderTwo.toWriter(writerTwo, propertiesTwo);
+
+            xmlFileBuilder.addElements(dataGuide.getModuleOneModuleTwoWeight());
+            XMLBuilder2 builderThree = xmlFileBuilder.getBuilder();
+            PrintWriter writer = new PrintWriter(modules);
+            Properties propertiesThree = xmlFileBuilder.getProperties();
+            builderThree.toWriter(writer, propertiesThree);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return new ResponseEntity(null, HttpStatus.BAD_REQUEST);
+        }
+
+
+        DocumentBuilderFactory documentBuilder = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = null;
+
+        Document filesXml;
+        Document methodsXml;
+        Document modulesXml;
+
+        try {
+            builder = documentBuilder.newDocumentBuilder();
+
+            filesXml = builder.parse(files);
+            methodsXml = builder.parse(methods);
+            modulesXml = builder.parse(methods);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Transformer transformer;
+        StringWriter writer = new StringWriter();
+
+        try {
+            transformer = factory.newTransformer();
+
+            transformer.transform(new DOMSource(filesXml), new StreamResult(writer));
+            model.setFiles(writer.toString());
+
+            writer.flush();
+
+            transformer.transform(new DOMSource(methodsXml), new StreamResult(writer));
+            model.setMethods(writer.toString());
+
+            writer.flush();
+
+            transformer.transform(new DOMSource(modulesXml), new StreamResult(writer));
+            model.setModules(writer.toString());
+
+            writer.flush();
+
+
+        } catch (TransformerException e) {
+            e.printStackTrace();
+            return new ResponseEntity(null, HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity(gson.toJson(model), HttpStatus.OK);
+
+    }
+
 }
+
